1c791bae01e1cabc4ae48389a95d9458914003ac
rahulgoel-developer <goelrahul78@gmail.com>
2023-04-13 16:37:16 +0530

    update yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rahul Edited <code>src/App.js</code> and save to reload.
+          Rahul Edit<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
