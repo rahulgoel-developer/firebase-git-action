7c27d08c4962473c35240a1c9362cbd04a04a02b
rahulgoel-developer <goelrahul78@gmail.com>
2023-04-14 16:52:56 +0530

    update text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rahul Edited<code>src/App.js</code> and deploy the code on firebase
+         deploye on firebase
         </p>
         <a
           className="App-link"
