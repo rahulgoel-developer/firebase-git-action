13a5da34a2c532e61a543eb6a22e3501c4a40b1f
rahulgoel-developer <goelrahul78@gmail.com>
2023-04-14 12:54:17 +0530

    sahil

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rahul Edited<code>src/App.js</code> and save 
+          Rahul Edited<code>src/App.js</code> and 
         </p>
         <a
           className="App-link"
